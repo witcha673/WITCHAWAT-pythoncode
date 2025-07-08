2415b399af99190f65c5b97d33b55f6b6cc14c76
Witchawat Supavananimit <witchawat.s@ku.th>
2025-07-08 12:01:42 +0700

    I do my home work in folder week01

---
+++ assignments/week01/Lap01.py
@@ -0,0 +1,18 @@
+
+Fullname = input("Enter name surname:")
+age = int(input("Enter your age:"))
+email = input("Enter your email:")
+phonenumber = int(input("Enter your phone number:"))
+hobby = input("Enter your favorite hobby:")
+
+print("your profile")
+print(f"Your name is {Fullname}")
+print(f"Your age is {age}")
+print(f"Your email is {email}")
+print(f"Your phone number is {phonenumber}")
+print(f"Your hobby is {hobby}")
+
+
+
+
+
