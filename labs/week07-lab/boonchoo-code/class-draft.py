6ad344064727ea416ca071ab5a9d46c644839bf0
boonchoo jitnupong <jitnupong.b@ku.th>
2025-09-03 21:58:19 +0700

    add oop week1 materials

---
+++ labs/week07-lab/boonchoo-code/class-draft.py
@@ -0,0 +1,10 @@
+class ClassName:
+    """Class docstring"""
+    
+    def __init__(self, parameters):
+        # Constructor method
+        self.attribute = value
+    
+    def method_name(self):
+        # Instance method
+        return something
